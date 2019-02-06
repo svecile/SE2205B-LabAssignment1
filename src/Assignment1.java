5e11268cad709cca5e4d5e168f86d029a3256ccb
Spencer <svecile@uwo.ca>
2019-02-06 14:17:15 -0500

    created project

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,36 +1,32 @@
 import java.io.*;
 
-public class Assignment1{
-  
-  public int[][] denseMatrixMult(int[][] A, int[][] B, int size)
-  {
+public class Assignment1 {
 
-  }
-  
-  public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
-  {
- 
-  }
-  
-  public int[][] sub(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n)
-  {
+    private int matrix[][];
 
-  }
-  
-  
-  public int[][] initMatrix(int n)
-  {
+    public int[][] denseMatrixMult(int[][] A, int[][] B, int size) {
 
-  }
-  
-  public void printMatrix(int n, int[][] A)
-  {
- 
-  }
-  
-  public int[][] readMatrix(String filename, int n) throws Exception
-  {
- 
-  }
-  
+    }
+
+    public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n) {
+
+    }
+
+    public int[][] sub(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n) {
+
+    }
+
+
+    public int[][] initMatrix(int n) {
+    
+    }
+
+    public void printMatrix(int n, int[][] A) {
+
+    }
+
+    public int[][] readMatrix(String filename, int n) throws Exception {
+
+    }
+
 }
