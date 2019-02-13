2d33bbb7f997527538fa1337ba60d6924a0a6ddb
s7560m <haydenhoffman@gmail.com>
2019-02-13 13:04:13 -0500

    Finished recursive code

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -5,14 +5,117 @@ public class Assignment1 {
 
     private int matrix[][];
 
-    public int[][] denseMatrixMult(int[][] A, int[][] B, int size) {
-        return A;
+//    // Private strassen algorithm to return multiplied matrix
+//    // @params are 2x2 matrices
+//    // @return 2x2 matrix of strassen-computed matrices
+//    private int[][] strassenCompute(int[][] a, int[][] b) {
+//
+//        return new int[5][6];
+//    }
+    private int nextPowerOfTwo(int n) {
+        int log2 = (int) Math.ceil(Math.log(n) / Math.log(2));
+        return (int) Math.pow(2, log2);
+    }
+
+    /** Function to multiply matrices **/
+    public int[][] denseMatrixMult(int[][] A, int[][] B, int size)
+    {
+        int n = size;
+        int[][] R = new int[n][n];
+        // initialization
+        if (n == 1)
+            R[0][0] = A[0][0] * B[0][0];
+        else
+        {
+            int[][] A11 = new int[n/2][n/2];
+            int[][] A12 = new int[n/2][n/2];
+            int[][] A21 = new int[n/2][n/2];
+            int[][] A22 = new int[n/2][n/2];
+            int[][] B11 = new int[n/2][n/2];
+            int[][] B12 = new int[n/2][n/2];
+            int[][] B21 = new int[n/2][n/2];
+            int[][] B22 = new int[n/2][n/2];
+
+            // Split matrix A into 4
+            split(A, A11, 0 , 0, n/2);
+            split(A, A12, 0 , n/2, n/2);
+            split(A, A21, n/2, 0, n/2);
+            split(A, A22, n/2, n/2, n/2);
+            // Split matrix B into 4
+            split(B, B11, 0 , 0, n/2);
+            split(B, B12, 0 , n/2, n/2);
+            split(B, B21, n/2, 0, n/2);
+            split(B, B22, n/2, n/2, n/2);
+
+            /**
+             M1 = (A11 + A22)(B11 + B22)
+             M2 = (A21 + A22) B11
+             M3 = A11 (B12 - B22)
+             M4 = A22 (B21 - B11)
+             M5 = (A11 + A12) B22
+             M6 = (A21 - A11) (B11 + B12)
+             M7 = (A12 - A22) (B21 + B22)
+             **/
+            int[][] m1_0 = sum(A11, A22,0,0,0,0,n/2);
+            int[][] m1_1 = sum(B11, B22,0,0,0,0,n/2);
+            int [][] M1 = denseMatrixMult(m1_0, m1_1, n/2);
+
+            int[][] m2_0 = sum(A21, A22,0,0,0,0,n/2);
+            int [][] M2 = denseMatrixMult(m2_0, B11, n/2);
+
+            int[][] m3_0 = sub(B12, B22,0,0,0,0,n/2);
+            int [][] M3 = denseMatrixMult(A11, m3_0, n/2);
+
+            int[][] m4_0 = sub(B21, B11,0,0,0,0,n/2);
+            int [][] M4 = denseMatrixMult(A22, m4_0, n/2);
+
+            int[][] m5_0 = sum(A11, A12,0,0,0,0,n/2);
+            int [][] M5 = denseMatrixMult(m5_0, B22, n/2);
+
+            int[][] m6_0 = sub(A21, A11,0,0,0,0,n/2);
+            int[][] m6_1 = sum(B11, B12,0,0,0,0,n/2);
+            int [][] M6 = denseMatrixMult(m6_0, m6_1, n/2);
+
+            int[][] m7_0 = sub(A12, A22,0,0,0,0,n/2);
+            int[][] m7_1 = sum(B21, B22,0,0,0,0,n/2);
+            int [][] M7 = denseMatrixMult(m7_0, m7_1, n/2);
+
+            //C11 = M1 + M4 - M5 + M7
+            int [][] C11 = sum(sub(sum(M1, M4,0,0,0,0,n/2), M5,0,0,0,0,n/2), M7,0,0,0,0,n/2);
+            //C12 = M3 + M5
+            int [][] C12 = sum(M3, M5,0,0,0,0,n/2);
+            //C21 = M2 + M4
+            int [][] C21 = sum(M2, M4,0,0,0,0,n/2);
+            //C22 = M1 - M2 + M3 + M6
+            int [][] C22 = sum(sub(sum(M1, M3,0,0,0,0,n/2), M2,0,0,0,0,n/2), M6,0,0,0,0,n/2);
+
+            // Join matrices
+            join(C11, R, 0 , 0, n/2);
+            join(C12, R, 0 , n/2, n/2);
+            join(C21, R, n/2, 0, n/2);
+            join(C22, R, n/2, n/2, n/2);
+        }
+        return R;
+    }
+    // Split parent matrix into child matrices
+    public void split(int[][] P, int[][] C, int childBoundX, int childBoundY, int size)
+    {
+        for(int i = 0, j = childBoundX; i < size; i++, j++)
+            for(int k = 0, l = childBoundY; k < size; k++, l++)
+                C[i][k] = P[j][l];
+    }
+    // Join child matrices into one big parent matrix
+    public void join(int[][] C, int[][] P, int childBoundX, int childBoundY, int size)
+    {
+        for(int i = 0, j = childBoundX; i < size; i++, j++)
+            for(int k = 0, l = childBoundY; k < size; k++, l++)
+                P[j][l] = C[i][k];
     }
 
     public int[][] sum(int[][] A, int[][] B, int x1, int y1, int x2, int y2, int n) {
 
         int[][] sum = new int[n][n];
-
+        // sums columns together then rows
         for (int i = 0; i < n; i++) {
 
             int yA = y1, yB = y2;
